3ad284970c1f3b1d6c7a8ead66563a4b7b80801c
ragam <ragam@DESKTOP-8I1FF0G>
2024-07-01 10:15:35 +0530

    this is my identifier project

---
+++ coreJava/src/dayone/IdentifiersDemo.java
@@ -0,0 +1,18 @@
+package dayone;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int for=13;
+		//System.out.println("value of the number variable is:" for);
+		//int number2=13;
+		//System.out.println("value of the number variable is:"+ number2);
+		//int number3=10;
+		int $number=20;
+		System.out.println("value of the number variable is:" +$number);
+		String studentName="Aniket";
+		System.out.println("value is:"+studentName);
+	}
+
+}
