d525ff08d8bdadfb01d1a4600c126647810a24ca
ragam <ragam@DESKTOP-8I1FF0G>
2024-07-01 10:31:37 +0530

    this is my primitive datatype project

---
+++ coreJava/src/dayone/PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+package dayone;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println("Min range of byte is" +byteMin + "Max range of byte is" +byteMax);
+		short shortMax=32767;
+		short shortMin=-31957;
+		System.out.println("Minshort range of byte is" +shortMin + "Maxshort range byte is" +shortMax);
+		int maxInt = 2143759209;
+		int minInt = -2143759207;
+		System.out.println("MinInt range of byte is" +minInt+ "MaxInt range of byte is" +maxInt);
+		long maxLong = 9223372036854775807L;
+		long minLong =  -9223372036854775807L;
+		System.out.println("Minlong range of byte is " +minLong + "Maxlong range of byte is " +maxLong);
+	}
+
+}
